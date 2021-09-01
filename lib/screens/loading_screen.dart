7fca0034869bb4e084a4d9a7ff6c7b5ad76b0dc9
Yabish Arfath <yabisharfath7@gmail.com>
2021-09-01 11:18:03 +0400

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    // LocationPermission permission;
+    // permission = await Geolocator.checkPermission();
+    // print(permission);
+
+    // Geolocator.openLocationSettings();
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => {
+            getLocation(),
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
